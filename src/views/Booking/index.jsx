18473e70c154f6db2ff3f1a17fc4e5767ec61ee6
Colin <cwh@colinwhill.com>
2018-11-18 16:04:54 +0100

    [fox] linter issues

--- src/views/Booking/index.jsx
+++ src/views/Booking/index.jsx
@@ -11,23 +11,32 @@ class App extends React.Component {
         <div className="app-main-form-background app-main-form-background-bookings">
           <form onSubmit={() => {}} className="app-main-form app-main-col_1">
             <div className="app-main-col_1">
-              <h4 className="app-main-h4">View a booking</h4>
-            </div>
-
-             <div className="app-main-col">
-               <Input className="app-main-form-input" placeholder="Booking reference" />
+              <h4 className="app-main-h4">
+                View a booking
+              </h4>
             </div>
 
             <div className="app-main-col">
-              <Button className="app-main-form-button_color">Search</Button>
+              <Input
+                className="app-main-form-input"
+                placeholder="Booking reference"
+              />
+            </div>
+
+            <div className="app-main-col">
+              <Button className="app-main-form-button_color">
+                Search
+              </Button>
             </div>
           </form>
         </div>
 
         <div className="app-main-wrapper">
         <div className="app-main-col_1 app-main-bookings">
-          <h4 className="app-main-h4">Booking id: 23345HDGFA34</h4>
-          <FlightCard/>
+          <h4 className="app-main-h4">
+            Booking id: 23345HDGFA34
+          </h4>
+          <FlightCard />
         </div>
         </div>
       </div>
