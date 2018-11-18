daba8eca91dee7573ffe38994cd2ca5ef2aabc62
Colin <cwh@colinwhill.com>
2018-11-18 20:03:36 +0100

    [update]  Booking view with redux endpoints

--- src/views/Booking/Booking.jsx
+++ src/views/Booking/Booking.jsx
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Input, Button } from 'reactstrap';
-import FlightCard from '../../components/FlightCard/index'
+import FlightCard from '../../components/FlightCard/index';
 
-class App extends Component {
-  state = {}
+class Booking extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      booking: undefined,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.searchBooking = this.searchBooking.bind(this);
+  }
+
+  componentDidMount() {
+    const { getBooking } = this.props;
+    const id = this.history.pathname().split('/bookings/')[1];
+
+    getBooking(id);
+  }
+
+  static getDerivedStateFromProps(props) {
+    if (props.booking) {
+      this.setState({ booking: props.booking });
+    }
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+
+    const change = {};
+    change[e.target.name] = e.target.value;
+
+    this.setState(change);
+  }
+
+  searchBooking() {
+    const { getBooking } = this.props;
+    const { bookingReference } = this.state;
+
+    getBooking(bookingReference);
+  }
 
   render() {
+    const { booking } = this.state;
+
     return (
       <div className="app-main">
         <div className="app-main-form-background app-main-form-background-bookings">
@@ -20,11 +60,16 @@ class App extends Component {
               <Input
                 className="app-main-form-input"
                 placeholder="Booking reference"
+                name="bookingReference"
+                onChange={this.handleChange}
               />
             </div>
 
             <div className="app-main-col">
-              <Button className="app-main-form-button_color">
+              <Button
+                className="app-main-form-button_color"
+                onClick={this.searchBooking}
+              >
                 Search
               </Button>
             </div>
@@ -35,8 +80,9 @@ class App extends Component {
         <div className="app-main-col_1 app-main-bookings">
           <h4 className="app-main-h4">
             Booking id: 23345HDGFA34
+            {booking.reference}
           </h4>
-          <FlightCard />
+          <FlightCard flight={booking.flight} />
         </div>
         </div>
       </div>
@@ -44,4 +90,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(Booking);
