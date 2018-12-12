da47363d61441dd7713329f10d22fc272e44745b
Colin <cwh@colinwhill.com>
2018-12-12 19:17:41 +0100

    [fix] logo to redirect to home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,10 +7,12 @@ import logo from '../../img/around.png';
 // The Header creates link to go back home (on logo)
 const Header = () => (
   <header className="app-header">
-   <div>
-   <img className="app-header-logo" src={logo} alt="logo"/>
-   <h2 className="app-header-h2">DreamFlights</h2>
-   </div>
+    <div>
+      <Link to="/">
+        <img className="app-header-logo" src={logo} alt="logo" />
+        <h2 className="app-header-h2">DreamFlights</h2>
+      </Link>
+    </div>
   </header>
 );
 
