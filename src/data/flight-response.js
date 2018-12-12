cfa8518cb1abfb9f306528d3c4e2abaa25acc05c
Colin <cwh@colinwhill.com>
2018-12-12 21:54:47 +0100

    [update] demo flight data

--- src/data/flight-response.js
+++ src/data/flight-response.js
@@ -1,190 +1,198 @@
 /* eslint-disable */
 const flightResponse = [
   {
-    departureFlight: {
-      id: 2,
-      airline: {
-        id: 8,
-        name: 'SAS',
+    "departureFlight": {
+      "id": 2,
+      "airlineIndex": 8,
+      "flightNumber": "SAS-2345",
+      "departureDateTime": "2018-12-01T06:11:11.012Z",
+      "arrivalDateTime": "2018-12-01T18:11:11.012Z",
+      "destinationIndex": 7,
+      "originIndex": 8,
+      "createdAt": "2018-12-03T20:30:11.144Z",
+      "updatedAt": "2018-12-03T20:30:11.144Z",
+      "airline": {
+        "id": 8,
+        "name": "SAS",
+        "createdAt": "2018-12-03T20:10:22.083Z",
+        "updatedAt": "2018-12-03T20:10:22.083Z"
       },
-      flightNumber: 'SAS-2345',
-      departureDateTime: '2018-12-01T06:11:11.012Z',
-      arrivalDateTime: '2018-12-01T18:11:11.012Z',
-      destination: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
+      "destination": {
+        "id": 7,
+        "name": "John F. Kennedy International Airport",
+        "code": "JFK",
+        "createdAt": "2018-11-11T23:51:20.504Z",
+        "updatedAt": "2018-11-11T23:51:20.504Z"
       },
-      origin: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
-      },
-      createdAt: '2018-12-03T20:30:11.144Z',
-      updatedAt: '2018-12-03T20:30:11.144Z',
+      "origin": {
+        "id": 8,
+        "name": "Copenhagen Airport Kastrup",
+        "code": "CPH",
+        "createdAt": "2018-12-03T20:11:59.900Z",
+        "updatedAt": "2018-12-03T20:11:59.900Z"
+      }
     },
-    returnFlight: {
-      id: 6,
-      airline: {
-        id: 8,
-        name: 'SAS',
+    "returnFlight": {
+      "id": 6,
+      "airlineIndex": 8,
+      "flightNumber": "SAS-2351",
+      "departureDateTime": "2018-12-24T06:11:11.012Z",
+      "arrivalDateTime": "2018-12-24T18:00:11.012Z",
+      "destinationIndex": 8,
+      "originIndex": 7,
+      "createdAt": "2018-12-03T20:34:43.373Z",
+      "updatedAt": "2018-12-03T20:34:43.373Z",
+      "airline": {
+        "id": 8,
+        "name": "SAS",
+        "createdAt": "2018-12-03T20:10:22.083Z",
+        "updatedAt": "2018-12-03T20:10:22.083Z"
       },
-      flightNumber: 'SAS-2351',
-      departureDateTime: '2018-12-24T06:11:11.012Z',
-      arrivalDateTime: '2018-12-24T18:00:11.012Z',
-      destination: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
+      "destination": {
+        "id": 8,
+        "name": "Copenhagen Airport Kastrup",
+        "code": "CPH",
+        "createdAt": "2018-12-03T20:11:59.900Z",
+        "updatedAt": "2018-12-03T20:11:59.900Z"
       },
-      origin: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
-      },
-      createdAt: '2018-12-03T20:34:43.373Z',
-      updatedAt: '2018-12-03T20:34:43.373Z',
-    },
+      "origin": {
+        "id": 7,
+        "name": "John F. Kennedy International Airport",
+        "code": "JFK",
+        "createdAt": "2018-11-11T23:51:20.504Z",
+        "updatedAt": "2018-11-11T23:51:20.504Z"
+      }
+    }
   },
   {
-    departureFlight: {
-      id: 2,
-      airline: {
-        id: 8,
-        name: 'SAS',
+    "departureFlight": {
+      "id": 2,
+      "airlineIndex": 8,
+      "flightNumber": "SAS-2345",
+      "departureDateTime": "2018-12-01T06:11:11.012Z",
+      "arrivalDateTime": "2018-12-01T18:11:11.012Z",
+      "destinationIndex": 7,
+      "originIndex": 8,
+      "createdAt": "2018-12-03T20:30:11.144Z",
+      "updatedAt": "2018-12-03T20:30:11.144Z",
+      "airline": {
+        "id": 8,
+        "name": "SAS",
+        "createdAt": "2018-12-03T20:10:22.083Z",
+        "updatedAt": "2018-12-03T20:10:22.083Z"
       },
-      flightNumber: 'SAS-2345',
-      departureDateTime: '2018-12-01T06:11:11.012Z',
-      arrivalDateTime: '2018-12-01T18:11:11.012Z',
-      destination: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
+      "destination": {
+        "id": 7,
+        "name": "John F. Kennedy International Airport",
+        "code": "JFK",
+        "createdAt": "2018-11-11T23:51:20.504Z",
+        "updatedAt": "2018-11-11T23:51:20.504Z"
       },
-      origin: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
-      },
-      createdAt: '2018-12-03T20:30:11.144Z',
-      updatedAt: '2018-12-03T20:30:11.144Z',
+      "origin": {
+        "id": 8,
+        "name": "Copenhagen Airport Kastrup",
+        "code": "CPH",
+        "createdAt": "2018-12-03T20:11:59.900Z",
+        "updatedAt": "2018-12-03T20:11:59.900Z"
+      }
     },
-    returnFlight: {
-      id: 6,
-      airline: {
-        id: 8,
-        name: 'SAS',
+    "returnFlight": {
+      "id": 6,
+      "airlineIndex": 8,
+      "flightNumber": "SAS-2351",
+      "departureDateTime": "2018-12-24T06:11:11.012Z",
+      "arrivalDateTime": "2018-12-24T18:00:11.012Z",
+      "destinationIndex": 8,
+      "originIndex": 7,
+      "createdAt": "2018-12-03T20:34:43.373Z",
+      "updatedAt": "2018-12-03T20:34:43.373Z",
+      "airline": {
+        "id": 8,
+        "name": "SAS",
+        "createdAt": "2018-12-03T20:10:22.083Z",
+        "updatedAt": "2018-12-03T20:10:22.083Z"
       },
-      flightNumber: 'SAS-2351',
-      departureDateTime: '2018-12-24T06:11:11.012Z',
-      arrivalDateTime: '2018-12-24T18:00:11.012Z',
-      destination: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
+      "destination": {
+        "id": 8,
+        "name": "Copenhagen Airport Kastrup",
+        "code": "CPH",
+        "createdAt": "2018-12-03T20:11:59.900Z",
+        "updatedAt": "2018-12-03T20:11:59.900Z"
       },
-      origin: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
-      },
-      createdAt: '2018-12-03T20:34:43.373Z',
-      updatedAt: '2018-12-03T20:34:43.373Z',
-    },
+      "origin": {
+        "id": 7,
+        "name": "John F. Kennedy International Airport",
+        "code": "JFK",
+        "createdAt": "2018-11-11T23:51:20.504Z",
+        "updatedAt": "2018-11-11T23:51:20.504Z"
+      }
+    }
   },
   {
-    departureFlight: {
-      id: 2,
-      airline: {
-        id: 8,
-        name: 'SAS',
+    "departureFlight": {
+      "id": 2,
+      "airlineIndex": 8,
+      "flightNumber": "SAS-2345",
+      "departureDateTime": "2018-12-01T06:11:11.012Z",
+      "arrivalDateTime": "2018-12-01T18:11:11.012Z",
+      "destinationIndex": 7,
+      "originIndex": 8,
+      "createdAt": "2018-12-03T20:30:11.144Z",
+      "updatedAt": "2018-12-03T20:30:11.144Z",
+      "airline": {
+        "id": 8,
+        "name": "SAS",
+        "createdAt": "2018-12-03T20:10:22.083Z",
+        "updatedAt": "2018-12-03T20:10:22.083Z"
       },
-      flightNumber: 'SAS-2345',
-      departureDateTime: '2018-12-01T06:11:11.012Z',
-      arrivalDateTime: '2018-12-01T18:11:11.012Z',
-      destination: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
+      "destination": {
+        "id": 7,
+        "name": "John F. Kennedy International Airport",
+        "code": "JFK",
+        "createdAt": "2018-11-11T23:51:20.504Z",
+        "updatedAt": "2018-11-11T23:51:20.504Z"
       },
-      origin: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
-      },
-      createdAt: '2018-12-03T20:30:11.144Z',
-      updatedAt: '2018-12-03T20:30:11.144Z',
+      "origin": {
+        "id": 8,
+        "name": "Copenhagen Airport Kastrup",
+        "code": "CPH",
+        "createdAt": "2018-12-03T20:11:59.900Z",
+        "updatedAt": "2018-12-03T20:11:59.900Z"
+      }
     },
-    returnFlight: {
-      id: 6,
-      airline: {
-        id: 8,
-        name: 'SAS',
+    "returnFlight": {
+      "id": 6,
+      "airlineIndex": 8,
+      "flightNumber": "SAS-2351",
+      "departureDateTime": "2018-12-24T06:11:11.012Z",
+      "arrivalDateTime": "2018-12-24T18:00:11.012Z",
+      "destinationIndex": 8,
+      "originIndex": 7,
+      "createdAt": "2018-12-03T20:34:43.373Z",
+      "updatedAt": "2018-12-03T20:34:43.373Z",
+      "airline": {
+        "id": 8,
+        "name": "SAS",
+        "createdAt": "2018-12-03T20:10:22.083Z",
+        "updatedAt": "2018-12-03T20:10:22.083Z"
       },
-      flightNumber: 'SAS-2351',
-      departureDateTime: '2018-12-24T06:11:11.012Z',
-      arrivalDateTime: '2018-12-24T18:00:11.012Z',
-      destination: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
+      "destination": {
+        "id": 8,
+        "name": "Copenhagen Airport Kastrup",
+        "code": "CPH",
+        "createdAt": "2018-12-03T20:11:59.900Z",
+        "updatedAt": "2018-12-03T20:11:59.900Z"
       },
-      origin: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
-      },
-      createdAt: '2018-12-03T20:34:43.373Z',
-      updatedAt: '2018-12-03T20:34:43.373Z',
-    },
+      "origin": {
+        "id": 7,
+        "name": "John F. Kennedy International Airport",
+        "code": "JFK",
+        "createdAt": "2018-11-11T23:51:20.504Z",
+        "updatedAt": "2018-11-11T23:51:20.504Z"
+      }
+    }
   },
-  {
-    departureFlight: {
-      id: 2,
-      airline: {
-        id: 8,
-        name: 'SAS',
-      },
-      flightNumber: 'SAS-2345',
-      departureDateTime: '2018-12-01T06:11:11.012Z',
-      arrivalDateTime: '2018-12-01T18:11:11.012Z',
-      destination: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
-      },
-      origin: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
-      },
-      createdAt: '2018-12-03T20:30:11.144Z',
-      updatedAt: '2018-12-03T20:30:11.144Z',
-    },
-    returnFlight: {
-      id: 6,
-      airline: {
-        id: 8,
-        name: 'SAS',
-      },
-      flightNumber: 'SAS-2351',
-      departureDateTime: '2018-12-24T06:11:11.012Z',
-      arrivalDateTime: '2018-12-24T18:00:11.012Z',
-      destination: {
-        id: 8,
-        name: 'New York John F. Kennedy',
-        code: 'JFK',
-      },
-      origin: {
-        id: 7,
-        name: 'Copenhagen Kastrup',
-        code: 'CPH',
-      },
-      createdAt: '2018-12-03T20:34:43.373Z',
-      updatedAt: '2018-12-03T20:34:43.373Z',
-    },
-  },
-];
+]
 
 export {
   flightResponse,
