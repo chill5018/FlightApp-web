29a9a18b42ca1eb49ced799e1b4593773ed6d7ad
Colin <cwh@colinwhill.com>
2018-12-12 21:50:33 +0100

    [add] date helpers and tests

--- src/__tests__/helpers.spec.js
+++ src/__tests__/helpers.spec.js
@@ -1,4 +1,4 @@
-import { filterDropdownOptions } from '../utils/helpers';
+import { filterDropdownOptions, calculateDuration, convertToTime } from '../utils/helpers';
 
 describe('Helpers', () => {
   it('filter dropdown options', () => {
@@ -9,6 +9,44 @@ describe('Helpers', () => {
     ];
 
     const result = filterDropdownOptions(options, options[0]);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('calculates duration properly', () => {
+    const expectedResult = '10h 21m';
+    const arrivalDateTime = '2018-12-01T16:32:11.012Z';
+    const departureDateTime = '2018-12-01T06:11:11.012Z';
+
+    const result = calculateDuration(departureDateTime, arrivalDateTime);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('calculates really long durations properly', () => {
+    const expectedResult = '34h 21m';
+    const arrivalDateTime = '2018-12-02T16:32:11.012Z';
+    const departureDateTime = '2018-12-01T06:11:11.012Z';
+
+    const result = calculateDuration(departureDateTime, arrivalDateTime);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('if duration is negative throw error', () => {
+    const arrivalDateTime = '2018-12-01T16:32:11.012Z';
+    const departureDateTime = '2018-12-02T06:11:11.012Z';
+    const expectedResult = new Error(`Invalid Departure / Arrival Dates: Departure: ${departureDateTime}, Arrival: ${arrivalDateTime}`);
+
+    expect(() => calculateDuration(departureDateTime, arrivalDateTime))
+      .toThrowError(expectedResult);
+  });
+
+  it('converts dates to time properly', () => {
+    const expectedResult = '16:32';
+    const arrivalDateTime = '2018-12-01T16:32:11.012Z';
+
+    const result = convertToTime(arrivalDateTime);
 
     expect(result).toEqual(expectedResult);
   });
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,32 @@
 import remove from 'lodash/remove';
+import moment from 'moment';
 
 const filterDropdownOptions = (options, prevSelected) => (
   remove(options, el => el !== prevSelected)
 );
 
+const calculateDuration = (departureDateTime, arrivalDateTime) => {
+  const departureDate = moment(departureDateTime);
+  const arrivalDate = moment(arrivalDateTime);
+
+  const duration = moment.duration(arrivalDate.diff(departureDate)).asMinutes();
+
+  const hours = Math.floor(duration / 60);
+  const minutes = duration % 60;
+
+  if (hours < 0) {
+    throw new Error(`Invalid Departure / Arrival Dates: Departure: ${departureDateTime}, Arrival: ${arrivalDateTime}`);
+  }
+
+  return `${hours}h ${minutes}m`;
+};
+
+const convertToTime = dateTimeString => moment(dateTimeString).utcOffset(0).format('HH:mm');
+
+
 /* eslint-disable import/prefer-default-export */
 export {
+  calculateDuration,
+  convertToTime,
   filterDropdownOptions,
 };
