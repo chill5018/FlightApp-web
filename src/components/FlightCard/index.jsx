b048a89efe874d40bdfa2bbb8351eed8b9767e71
Colin <cwh@colinwhill.com>
2018-12-17 16:55:53 +0100

    [remove] unused import

--- src/components/FlightCard/index.jsx
+++ src/components/FlightCard/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
 import { convertToTime, calculateDuration, generatePrice } from '../../utils/helpers';
 
 
